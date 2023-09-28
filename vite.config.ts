715f6d23d7ca2fd3b70372dd5f727e4cef228fb8
Jan <jan@audoora.de>
2023-09-28 14:57:18 +0200

    moved output to docs

--- vite.config.ts
+++ vite.config.ts
@@ -13,5 +13,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: "/anya-configurator/"
+  base: "/anya-configurator/",
+  build: {
+    outDir: "docs"
+  }
 })
